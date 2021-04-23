9b70a35bbb6fe8610713c86528e3e424098a5e02
Thomas Olsson <olss.thom@gmail.com>
2021-04-24 01:18:55 +0200

    prepares book button click handler

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,17 @@
+// import Booking from "./Booking";
+
 function Card({ img, name, description, price }) {
+
+    function handleClick(e) {
+        console.log('Button clicked');
+    }
+
     return (
         <article>
             <img />
             <h2>{name} </h2>
             <p>{description}</p>
-            <button>Boka</button>
+            <button onClick={handleClick}>Boka</button>
             <p><data value={price}>{price}</data>sek</p>
         </article>
     )
