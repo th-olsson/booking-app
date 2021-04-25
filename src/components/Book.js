e947704f3df3a6bd18042207582a9486ccdf1302
Thomas Olsson <olss.thom@gmail.com>
2021-04-25 18:02:24 +0200

    sets state on form change

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,25 +1,69 @@
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom'
 
+
 function Book() {
+    const treatmentDetails = (useLocation().state);
+    const { name, category, description, id, price } = treatmentDetails;
 
-    const bookingDetails = (useLocation().state);
+    const [bookingDetails, setBookingDetails] = useState({
+        treatmentId: id,
+        name: '',
+        time: '',
+        tel: ''
+    });
+
+    console.log("initial booking details:");
     console.log(bookingDetails);
-    const {name, category, description, id, price} = bookingDetails;
 
-    function handleChange() {
-        console.log('Form changed');
+    function handleChange(e) {
+        console.log("former booking details:");
+        console.log(bookingDetails);
+
+        const newBookingDetails = bookingDetails;
+
+        const inputName = e.target.name;
+        const inputValue = e.target.value;
+
+        switch (inputName) {
+            case "name":
+                newBookingDetails.name = inputValue;
+                break;
+            case "time":
+                newBookingDetails.time = inputValue;
+                break;
+            case "tel":
+                newBookingDetails.tel = inputValue;
+                break;
+            default:
+                console.log("No match");
+                return;
+        }
+
+        console.log("new booking details:");
+        console.log(newBookingDetails);
+
+        setBookingDetails(newBookingDetails);
     }
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(e.target.text.value)
+    }
+
+
     return (
-        <form onChange={handleChange}>
+        <>
             <h2>Boka {name} {price} sek</h2>
-            <label htmlFor='text'>Ditt namn</label>
-            <input type='text' name='text' />
-            <label htmlFor='time'>Önskad tid</label>
-            <input type='time' name='time' />
-            <input type='tel' name='tel' placeholder='Telefonnummer' />
-            <button>Bekräfta</button>
-        </form>
+            <form onChange={handleChange} onSubmit={handleSubmit} method='post'>
+                <label htmlFor='text'>Ditt namn</label>
+                <input type='text' name='name' required />
+                <label htmlFor='time'>Önskad tid</label>
+                <input type='time' name='time' required />
+                <input type='tel' name='tel' placeholder='Telefonnummer' />
+                <button>Bekräfta</button>
+            </form>
+        </>
     )
 }
 
