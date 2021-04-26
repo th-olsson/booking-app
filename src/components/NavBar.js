6fff36ddff3fe5c5b4abd9072ee001ec05fa8148
Thomas Olsson <olss.thom@gmail.com>
2021-04-26 06:18:07 +0200

    basic design to navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,14 +2,17 @@ import { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
-        <nav>
-            <ul>
-                {/* <li><svg></svg></li> logo*/}
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/behandling/frisör'>Frisör</Link></li>
-                <li><Link to='/behandling/skönhet'>Skönhet</Link></li>
-                <li><Link to='/bokningar'>Mina bokningar</Link></li>
-                <li><Link to='/inloggning'>Login/logout</Link></li>
+        <nav className="bg-white px-8 py-1 flex flex-col">
+            <ul className="flex justify-between">
+                <div className="flex justify-between items-center">
+                    <li className="px-2 text-lg"><Link to='/'><img className src="https://img.icons8.com/bubbles/50/000000/star.png" /></Link></li>
+                    <li className="px-2 text-lg"><Link to='/behandling/frisör'>Frisör</Link></li>
+                    <li className="px-2 text-lg"><Link to='/behandling/skönhet'>Skönhet</Link></li>
+                </div>
+                <div className="flex items-center">
+                    <li className="px-2 text-lg"><Link to='/bokningar'>Mina bokningar</Link></li>
+                    <li className="px-2 text-lg "><Link to='/inloggning'>Logga in</Link></li>
+                </div>
             </ul>
         </nav>
     )
--- src/components/TreatmentList.js
+++ src/components/TreatmentList.js
@@ -84,7 +84,7 @@ function TreatmentList({ category }) {
     return (
         <>
             {/* <h1>Behandlingar inom {category}</h1> */}
-            <section className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+            <section className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 {treatments.map(({ id, name, description, price, category }) =>
 
                     currentUrlCategory === category &&
