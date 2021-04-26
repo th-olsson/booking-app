f94aa9a005a375559c9c874a93631d775b91d6d9
Thomas Olsson <olss.thom@gmail.com>
2021-04-26 20:40:55 +0200

    Revert "basic design to my home"
    This reverts commit 24de47a4c83a8335101c2f6717ecd1efe27ebf92.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,7 @@
-import { Link } from "react-router-dom"
-
 function Home() {
     return (
         <>
-            <h1  class="text-gray-800 text-3xl font-semibold text-center pb-10">Boka behandling</h1>
-            <div className="flex justify-evenly flex-wrap overflow-hidden">
-                <Link to='behandling/frisör/'>
-                    <img src='https://images.unsplash.com/photo-1593269233759-427ba69acca5?' height='500' width='400' />
-                    <h2 className="text-gray-750 text-lg pl-2 font-semibold">Frisör</h2>
-                </Link>
-                <Link to='behandling/frisör/'>
-                    <img src='https://images.unsplash.com/photo-1556760544-74068565f05c' height='500' width='400' />
-                    <h2 className="text-gray-750 text-lg pl-2 font-semibold">Skönhet</h2>
-                </Link>
-            </div>
+            <p>Välj behandling i navigationsfältet</p>
         </>
     )
 }
