031f2052a649ead8d421f73bbe6243747e72ba4a
Thomas Olsson <olss.thom@gmail.com>
2021-05-11 22:42:24 +0200

    reads treatments from db

--- src/components/Treatment.js
+++ src/components/Treatment.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-function Treatment({ value, img, name, description, price, category }) {
+function Treatment({ value, img, name, description, price, category, duration }) {
     const [treatmentInfo, seTreatmentInfo] = useState({
         id: value,
         name: name,
@@ -12,27 +12,31 @@ function Treatment({ value, img, name, description, price, category }) {
 
     return (
         // Card
-        <article className="flex flex-col justify-evenly rounded-md shadow-md overflow-hidden">
+        <article className="flex flex-col justify-evenly rounded-md shadow-md overflow-hidden text-center ">
             <img src="https://source.unsplash.com/random/500x400" />
             <div className="flex flex-col h-full justify-between">
-                <h2 className="text-gray-750 text-lg pl-2 font-semibold">{name} </h2>
-                <p className=" px-2 py-1">{description}</p>
-                <span className=" flex justify-around py-2">
-                    {/* Book-button */}
-                    <Link className="px-4 py-1 text-white font-light tracking-wider bg-gray-900 hover:bg-gray-800 rounded" to={
-                        {
-                            pathname: `/boka/${name.toLowerCase()}-${value}`,
-                            state: {
-                                id: value,
-                                name: name,
-                                description: description,
-                                price: price,
-                                category: category
-                            }
-                        }
-                    }>Boka</Link>
-                    <p className="text-xl"><data value={price}>{price}</data> SEK</p>
+                <h2 className="text-gray-900 text-lg pl-2 font-semibold">{name} </h2>
+                <span className=" flex justify-around border py-2 text-gray-800 text-sm pl-2 font-semibold">
+                    <p className="pr-1"><data value={price}>{price}kr </data></p>
+                    <span className="text-gray-500">/</span>
+                    <p className="">{duration}min</p>
                 </span>
+
+                <p className="px-2 py-1 ">{description}</p>
+                {/* Book-button */}
+                <Link className="px-4 py-1 text-gray-50 tracking-wider bg-green-700 hover:bg-green-600 rounded" to={
+                    {
+                        pathname: `/boka/${name.toLowerCase()}-${value}`,
+                        state: {
+                            id: value,
+                            name: name,
+                            description: description,
+                            price: price,
+                            category: category,
+                            duration: duration
+                        }
+                    }
+                }>Boka</Link>
             </div>
         </article>
     )
